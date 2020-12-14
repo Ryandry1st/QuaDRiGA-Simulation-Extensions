a22bd00471ad3d610b9b6563a31e6caea37f7b14
Ryan Dreifuerst <ryandry1st@utexas.edu>
2020-12-14 16:58:30 -0600

    Delete Tx_Sector_Information_from_csv.py

--- MRO/Tx_Sector_Information_from_csv.py
+++
@@ -1,9 +0,0 @@
-from Data_Helpers import get_tx_sector_orientations
-import sys
-
-def main():
-    filename = sys.argv[1]
-    print(get_tx_sector_orientations(filename))
-
-if __name__ == "__main__":
-    main()
