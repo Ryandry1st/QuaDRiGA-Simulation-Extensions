072d6a54f6540c476552eba7b8d338c50ee508cc
ali yazdan <ayp@fb.com>
2021-02-13 22:40:07 -0600

    carrier freq to 2GHz

--- CCO/MATLAB_Configurer.m
+++ CCO/MATLAB_Configurer.m
@@ -5,13 +5,13 @@ config = {};
 
 config.simulation.sim_num = '0.6'; % should be a string
 config.simulation.seed = 0;
-config.simulation.carrier_frequency_Mhz = 2800.0;
+config.simulation.carrier_frequency_Mhz = 2000.0;
 config.simulation.sampling_frequency_hz = 1000;
 config.simulation.samples = 40000;
 config.simulation.bandwidth_Mhz = 10;
 config.simulation.resource_blocks = 50;
 config.simulation.simulation_duration_s = 40;
-config.simulation.scenario = '3GPP_38.901_UMa'; %'Freespace', '3GPP_38.901_UMa'
+config.simulation.scenario = '3GPP_3D_UMa'; %'Freespace', '3GPP_38.901_UMa', '3GPP_3D_UMa'
 config.simulation.CCO_0_MRO_1 = 0; % set to 1 for MRO
 config.simulation.no_tx = 7;
 config.simulation.isd = 500; % intersite distance
@@ -20,7 +20,7 @@ config.simulation.no_rx_min = 20000;
 config.simulation.BS_drop = 'hex'; % Choose 'hex', 'rnd', 'csv' for built in layouts
 config.simulation.batch_tilts = [2,4,6,8,10,12,14,16];
 config.simulation.parallel = 1; % Set to 1 to enable parallel operation
-config.simulation.run_i = '3GPP_38.901_UMa';
+config.simulation.run_i = '3GPP_3D_UMa';
 
 config.UE(1).name = 'UE_1';
 config.UE(1).initial_position = [100, -200, 1.5];
